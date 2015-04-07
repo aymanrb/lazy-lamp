d323a69435e249d3d13d02a9a52dae1750e217f0
aymanrb <aymanrb@gmail.com>
2015-04-07 22:26:57 +0200

    Added mod pagespeed to the default apache2 installation script

--- Apache/install-apache2-mods.sh
+++ Apache/install-apache2-mods.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-# Installs few apache2 modules we usually need (Mod-ReWrite) 
+# Installs few apache2 modules we usually need (Mod-ReWrite / ModPageSpeed) 
 #
 # @aymanrb - 2015 / 04 / 07
 #
@@ -9,6 +9,14 @@ sudo -v
 
 echo "Installing Apache2 ReWrite Module"
 sudo a2enmod rewrite
+
+
+echo "Download Apache2 Google Mod-PageSpeed"
+cd /usr/local/src
+wget https://dl-ssl.google.com/dl/linux/direct/mod-pagespeed-stable_current_amd64.deb
+apt-get -f install
+dpkg -i mod-pagespeed-*.deb
+
 
 echo "Restarting Apache2 Service"
 service apache2 restart
