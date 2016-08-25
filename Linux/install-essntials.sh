1097e4d4ba7cd7860c63ae4b6efdd21aeac2f2a4
Ayman Bedair <aymanrb@gmail.com>
2016-08-25 14:27:54 +0200

    Adding Composer installation

--- Linux/install-essntials.sh
+++ Linux/install-essntials.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-# Script to automate the installation of few essential tools for Linux (Vim / GIT / )
+# Script to automate the installation of few essential tools for Linux (Vim / CURL / GIT / Composer)
 #
 # @aymanrb - 2014 / 07 / 21
 #
@@ -10,6 +10,9 @@ sudo -v
 echo "Installing VIM ..."
 sudo apt-get install vim
 
+echo "Installing CURL ..."
+sudo apt-get install curl
+
 echo "Installing GIT ..."
 sudo apt-get install git
 
@@ -18,5 +21,9 @@ read -p "Enter the User name of the Global GIT User: " MainUserName
 
 sudo git config --global user.email $MainUserMail
 sudo git config --global user.name  $MainUserName
+
+echo "Installing Composer ..."
+sudo apt-get install php5-cli
+curl -sS https://getcomposer.org/installer | sudo php -- --install-dir=/usr/local/bin --filename=composer
 
 echo "Done !"
