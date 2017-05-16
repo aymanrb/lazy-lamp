c1270eaf71f0589420787a9467c66130e63e76f2
ayman.bedair <ayman.bedair@contorion.de>
2017-05-16 11:12:51 +0200

    Removed the specified version of php modules

--- Php/install_modules.sh
+++ Php/install_modules.sh
@@ -9,14 +9,14 @@ sudo -v
 sudo apt-get update
 
 echo "Installing Curl for PHP ..."
-sudo apt-get install php5-curl
+sudo apt-get install php-curl
 
 echo "Installing IMAP function for PHP ..."
-sudo apt-get install php5-imap
+sudo apt-get install php-imap
 
 echo "Installing Mcrypt"
-sudo apt-get install mcrypt php5-mcrypt
-sudo php5enmod mcrypt
+sudo apt-get install mcrypt php-mcrypt
+sudo phpenmod mcrypt
 
 echo "Restarting Apache2 Server ..."
 sudo /etc/init.d/apache2 restart
